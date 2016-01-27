cde1d7ef8a6b3115bafe8f638bfef3463c08fdec
khaosage <khaosage@gmail.com>
2016-01-27 16:10:38 +0000

    pep8 formatting

--- sensu/plugins/check-postgres-connections.py
+++ sensu/plugins/check-postgres-connections.py
@@ -21,12 +21,14 @@ STATE_WARNING = 1
 STATE_CRITICAL = 2
 STATE_UNKNOWN = 3
 
+
 def percentage(part, whole):
-  return 100 * int(part)/int(whole)
+    return 100 * int(part)/int(whole)
 
 
 def connect_database(dbname, dbuser):
-    conn = psycopg2.connect('dbname={0} user={1} host=localhost'.format(dbname, dbuser))
+    conn = psycopg2.connect('dbname={0} user={1} host=localhost'.format(dbname,
+                            dbuser))
 
     mcur = conn.cursor()
     mcur.execute("SHOW MAX_CONNECTIONS;")
@@ -50,40 +52,40 @@ def main():
     parser = optparse.OptionParser()
 
     parser.add_option('-w', '--warning',
-      default = None,
-      type    = "int",
-      dest    = 'warning',
-      help    = 'Warning level percentage')
+                      default=None,
+                      type="int",
+                      dest='warning',
+                      help='Warning level percentage')
 
     parser.add_option('-c', '--critical',
-      default = None,
-      type    = "int",
-      dest    = 'critical',
-      help    = 'Critical level percentage',)
+                      default=None,
+                      type="int",
+                      dest='critical',
+                      help='Critical level percentage',)
 
     parser.add_option('-d', '--database',
-      default = None,
-      dest    = 'dbname',
-      help    = 'Database Name',)
+                      default=None,
+                      dest='dbname',
+                      help='Database Name',)
 
     parser.add_option('-u', '--user',
-      default = None,
-      dest    = 'dbuser',
-      help    = 'Database User Name',)
+                      default=None,
+                      dest='dbuser',
+                      help='Database User Name',)
 
     (options, args) = parser.parse_args()
 
     if not options.dbname:
-      parser.error('Please Specify a postgres database for connection')
+        parser.error('Please Specify a postgres database for connection')
 
     if not options.dbuser:
-      parser.error('Please Specify a postgres user for connection')
+        parser.error('Please Specify a postgres user for connection')
 
     if not options.warning:
-      parser.error('A Warning Level is required')
+        parser.error('A Warning Level is required')
 
     if not options.critical:
-      parser.error('A Critical Level is required')
+        parser.error('A Critical Level is required')
 
     percentage_connection = connect_database(options.dbname, options.dbuser)
 
@@ -95,5 +97,5 @@ def main():
         exit(STATE_OK)
 
 
-if '__main__' == __name__:
-  main()
+if __name__ == '__main__':
+    main()
